27f3035385e905b0cac9b37c33c60dc233a3f4a9
YunJiSang <js_7744@naver.com>
2021-11-16 23:07:30 +0900

    check

--- SDKSampleApp/Source/build/Win32/Debug/run.bat
+++ SDKSampleApp/Source/build/Win32/Debug/run.bat
@@ -1,2 +1,2 @@
-@set PATH=%PATH%;..\..\..\..\SDK\Libraries\Debug\x64
+@set PATH=%PATH%
 SDKSampleApp  --server=https://mt1s.www.vivox.com/api2 --realm=mt1s.vivox.com --issuer=jisang2336-te04-dev --key=chap633 -m %*
---
+++ SDKSampleApp/Source/build/Win32/test.bat
@@ -0,0 +1,8 @@
+cd C:\Users\js774\Desktop\Debug
+
+for /l %%i in (1, 1, 3) do (
+
+start C:\Users\js774\Desktop\Debug\run.bat
+
+timeout /t 10
+)
